29eece50ee70f64a726083a8cec95df091f2c1ec
Ira <you@example.com>
2017-10-29 15:21:44 +0300

    create task

--- format_price.py
+++ format_price.py
@@ -1,5 +1,13 @@
 def format_price(price):
-    pass
+    try:
+        if not isinstance(price, int) and not isinstance(price, float):
+            price = float(price)
+        return "{:,.2f}".format(price).rstrip('0').rstrip('.,').replace(',', ' ')
+    except (ValueError, TypeError):
+        return 'Incorrect value'
+
 
 if __name__ == '__main__':
-    pass
+    while True:
+        price = input('Input price for formatting: ')
+        print(format_price(price))
--- tests.py
+++ tests.py
@@ -1,1 +1,25 @@
-# TODO place your tests here
+from format_price import format_price
+import unittest
+
+
+class TestCasePrice(unittest.TestCase):
+    def test_extra_zeros_case(self):
+        self.assertEqual(format_price(1120.00), '1 120')
+
+    def test_fraction_part_case(self):
+        self.assertEqual(format_price(0.0140), '0.01')
+
+    def test_big_price_case(self):
+        self.assertEqual(format_price(1120000.10), '1 120 000.1')
+
+    def test_string_case(self):
+        self.assertEqual(format_price('120300'), '120 300')
+
+    def test_letter_case(self):
+        self.assertEqual(format_price('120t00'), 'Incorrect value')
+
+    def test_list_case(self):
+        self.assertEqual(format_price([12000]), 'Incorrect value')
+
+if __name__ == '__main__':
+    unittest.main()
